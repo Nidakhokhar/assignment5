773f0ea932f141dbb66412aab227aa13d7a6ee0e
nidakhokhar <nidakhokhar.work@gmail.com>
2020-06-15 15:28:51 +0500

    now ready

---
+++ app.js
@@ -0,0 +1,233 @@
+
+
+//assignment 1
+// Question 1
+//alert("Good Morning");
+
+// Question 2
+//alert("Error! Please enter a valid password.");
+
+// Question 3
+//alert("Welcome to JS land..." + "\n" + "Happy Coding");
+
+
+// Question 4
+//alert("Welcome to JS land");
+//alert("Happy Coding!" + "\n" + "Prevent this page from creating additional dialogs");
+
+
+
+// Question 5
+//Generate the following message through browser’s developer console:
+
+
+//a = "Hello... I can run JS through my web browser's console";
+//console.log(a)
+
+                        //VARIABLES FOR STRINGS//
+
+
+//Question 1.Declare a variable called username.   
+          
+//var userName = "abc";
+
+
+
+//Qusetion 2.Declare a variable called myName & assign to it a string that represents your Full Name.
+//var myName = "Fullname";
+
+//Qusetion 3.a) Declare a JS variable, titled message.
+// var JS = "message";
+
+//Qusetion 3.b) Assign “Hello World” to variable message
+// var message = "Hello World";
+
+//Qusetion 3.c) Display the message in alert box.
+// var JS = "message";
+//alert(JS);
+
+//Qusetion 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+// var name = "Jhone Deo";
+// var exp = "15Years" ;
+// var stat = "Certified Mobile Application Development";
+// alert (name + "\n" + exp + "\n" + stat);
+
+
+
+//Question 5. Write a script to display the following alert using one JS variable
+//var a = ['Pizza', 'Pizz', 'Piz' , 'Pi' , 'P'];
+//var x;
+
+//for (x of a) {
+// document.write(x + "<br>");
+//}
+
+//Question 6. Declare a variable called email and assign to it a string that 
+//represents your Email Address(e.g. example@example.com). Show the blow mentioned
+ //message in an alert box.(Hint: use string concatenation)
+
+//var email = "example@example.com" ;
+//var a = "My email address is" ;
+//var b = a + " " + email;
+//alert(b)
+
+//Question 7. Declare a variable called book & give it the value 
+//“A smarter way to learn JavaScript”. Display the following message in an alert box
+//var a = "A smarter way to learn JavaScript" ;
+//alert(a);
+
+//var line = "Yah! I can write HTML content through JavaScript";
+//document.write(line);
+                            //VARIABLES FOR NUMBERS
+//Question 1.Declare a variable called age & assign to it your age. Show your age in an alert box.
+//var age = "I am 15 years old";
+//alert(age);
+
+//Question 2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page.
+ //Show his/her number of visits on your web page.
+ //For example: “You have visited this site N times”.
+ ////var visit = "You have visited this site 14times";
+//alert(visit);
+
+//3. Declare a variable called birthYear & assign to it your birth year.
+// Show the following message in your browser:
+//
+
+//var birthYear = +prompt("Enter your birthyear");
+//var b = "My birth year is " + birthYear + "<br>"+"Data type of my decleared variable is number"
+//document.write(b);
+//
+// Question 4. A visitor visits an online clothing store www.xyzClothing.com .
+//  Write a script to store in variables the following information: 
+//  a. Visitor’s name b. Product title c. Quantity i.e. 
+//  how many products a visitor wants to order Show the following message in your browser:
+//  “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+//var visitorName = "John Doe" ;
+//var productTitle = "T-shirt(s)" ;
+//var Quantity = 5 ;
+//var a = visitorName + " " +"odered" + " " + Quantity + " " + productTitle + " " + "on XYZ Clothing store.";
+//document.write(a);
+
+                    //VARIABLE NAMES: LEGAL & ILLEGAL
+ //Question  1. Declare 3 variables in one statement.
+// var username = "john" , var age = "33" var roll= "76"; 
+                    
+//Declare 5 legal & 5 illegal variable names. 
+//legal variable
+
+//_username
+//user_name
+//$userName
+// userName
+//user11
+
+//illegal variable
+
+//9username
+//@username
+//user@name
+//#username
+//"username"
+
+
+ //Question  3.3. Display this in your browser a)  
+ //A heading stating “Rules for naming JS variables” b) 
+  //Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable
+  // c) Variables must begin with a ______, ______ or _____. For example $name, _name or name 
+  // d) Variable names are case _________ 
+// e) Variable names should not be JS _________ 
+
+//var a = "a)  A heading stating “Rules for naming JS variables" ;
+//var b = "b)  Variable names can only contain ______, ______, ______ and ______ For example $name, _name or name";
+//var c = "d) Variable names are case _________ ";
+//var d = "e) Variable names should not be JS _________ "
+//document.write(a + "<br>" + b + "<br>" + c + "<br>" + d);
+
+
+//MATH EXPRESSIONS
+
+//Question  1. Write a program that take two numbers & add them in a new variable.
+// Show the result in your browser.  
+//var a = 5;
+//var b = 5;
+//var c = a+b;
+//document.write(c);
+
+//Question 2. Repeat task1 for subtraction, multiplication, division & modulus. 
+
+//substraction
+// var a = 25;
+// var b = 5;
+// var c = a-b;
+// document.write(c);
+
+//multiplication
+// var a = 5;
+// var b = 5;
+// var c = a*b;
+// document.write(c);
+
+//Division
+// var a = 15;
+// var b = 5;
+// var c = a/b;
+// document.write(c);
+
+//Modulus
+// var a = 76;
+// var b = 5;
+// var c = a%b;
+// document.write(c);
+
+//Question 3. 
+// 3. Do the following using JS Mathematic Expressions 
+
+//   d. Show the value of variable in your browser like “Initial value: 5”. 
+// var a = 5 ;
+// var b = "Initial value:" ;
+// document.write(b + " " + a) ;
+
+//   e. Increment the variable. 
+//   f. Show the value of variable in your browser like “Value after increment is: 6”. 
+// var a = 5 ;
+// var b = a++ ;
+// var c = "Value after increment is:" ;
+// document.write(c + " " + a) ;
+
+// g. Add 7 to the variable. 
+// h. Show the value of variable in your browser like “Value after addition is: 13”.
+
+// var a = 7 ;
+// var b = 6 ;
+// var c = a + b ;
+// var d = "Value after addition is:" ;
+// document.write(c) ;
+
+// i. Decrement the variable. 
+// j. Show the value of variable in your browser like “Value after decrement is: 12”.
+
+// var a = 13 ;
+// var b = a-- ;
+// var c = "Value after decrement is:" ;
+// document.write(c + " " + b) ;
+
+// k. Show the remainder after dividing the variable’s value by 3.
+// l. Output : “The remainder is : 0”.
+
+// var a =2 ;
+// var b = 2 ;
+// var c = a%b ;
+// document.write( "The remainder is : " + " " + c) ;
+
+// Question 4. Cost of one movie ticket is 600 PKR. 
+// Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets to a movie.
+//  Example output:
+
+// var ticket = +prompt("Number of ticket")
+// totalcost = 600 ;
+// tick = ticket*totalcost ;
+// document.write(tick) ;
+
+
+// Question 5. Write a script to display multiplication table of any number in your browser. E.g
