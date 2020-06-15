2f710e8d3fbe4b831f70f3c9c1b6fbf0fe1bab8e
Nidakhokhar <57321033+Nidakhokhar@users.noreply.github.com>
2020-06-15 23:43:36 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -231,3 +231,69 @@
 
 
 // Question 5. Write a script to display multiplication table of any number in your browser. E.g
+ 
+
+// 6. The Temperature Converter: It’s hot out! Let’s make a converter based on the steps here. 
+// a.  Store a Celsius temperature into a variable. b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+//  c. Now store a Fahrenheit temperature into a variable. d. Convert it to Celsius & output “NNoF is NNoC”
+// function cToF(celsius) 
+// {
+//   const cTemp = celsius;
+//   const cToFahr = cTemp * 9 / 5 + 32;
+//   const message = `${cTemp}\xB0C is ${cToFahr} \xB0F.`;
+//     document.write(message);
+// }
+
+// function fToC(fahrenheit) 
+// {
+//   const fTemp = fahrenheit;
+//   const fToCel = (fTemp - 32) * 5 / 9;
+//   const message = `${fTemp}\xB0F is ${fToCel}\xB0C.`;
+//     document.write(message);
+// } 
+// cToF(60);
+// fToC(45);
+
+
+
+// }
+// 7.  Write a program to implement checkout process of a shopping cart system for an e-commerce website. Store the following in variables 
+// MATH EXPRESSIONS | JAVASCRIPT 
+// a. Price of item 1 b. Price of item 2 c. Ordered quantity of item 1 d. Ordered Quantity of item 2 e. Shipping charges 
+// Compute the total cost & show the receipt in your browser
+//  var item1 = 650 ;
+// var qty_item1 = 3 ;
+// var item2 = 100 ;
+// var qty_item2 = 7 ;
+// var shipping = 100 ;
+// var a = item1*qty_item1 ;
+// var b = item2*qty_item2 ;
+// var c = a + b + shipping ;
+
+// document.write( "Price of item 1 is : 650" + " <br> " + "Quantity of item 1 is : 3 " + "<br>" + "Price of item 2 is : 100 " + " <br> " + "Quantity of item 2 is : 7 " + "<br>" + "Total cost of your order is " + c );
+
+
+
+// 8. Store total marks & marks obtained by a student in 2 variables.
+//  Compute the percentage & show the result in your browser
+// var totalMarks = prompt("Enter total marks") ;
+// var marksObtained = prompt("Marks marks") ;
+// var percent = totalMarks/marksObtained ;
+// var result = percent*100 ;
+// document.write( "Total Marks : " + "  " + totalMarks + "<br>" + " Marks obtained : " + "  " + marksObtained  + "<br>" + "Percentage : " + "  " + result + "%");
+
+// 5. Write a script to display multiplication table of any number in your browser. E.g
+
+// for(var i = 1; i <= 10; i++){
+//   document.write("4" + "x" + i + "=" + 4*i + "<br>")
+
+
+
+// 9. Assume we have 10 US dollars & 25 Saudi Riyals. 
+// Write a script to convert the total currency to Pakistani Rupees. Perform all calculations in a single expression. 
+// (Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee and 1 Saudi Riyal = 28 Pakistani Rupee) 
+// var dollar = 10*104.80 ;
+// var saudi = 25*28 ;
+// document.write("US dollar " + "  " + dollar + "<br>" + "Saudi Riyal" + "  " + saudi);
+
+
